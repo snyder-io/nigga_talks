2544cc17c3d3a37b97efd4666d866fce08766dfc
Tushar K A <tushar.whitecollar@gmail.com>
2024-06-22 22:39:39 +0530

    na

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,9 +1,12 @@
 import './list.css';
+import UserInfo from './userInfo/UserInfo.jsx';
+import ChatList from './chatList/ChatList.jsx';
 
 const List = () =>{
     return(
         <div className="list">
-            List
+            <UserInfo />
+            <ChatList />
         </div>
     )
 }
