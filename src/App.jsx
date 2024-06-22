a88e9c43735ed743fa24afeeff1a67564a860edb
Tushar K A <156685841+snyder-io@users.noreply.github.com>
2024-06-22 15:16:39 +0000

    setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import List from "./components/list/List.jsx";
+import Chat from "./components/chat/Chat.jsx";
+import Detail from "./components/detail/Detail.jsx";
 
-function App() {
 
-  return (
-    <div className="container">
-      
-    </div>
-  )
+const App = () =>{
+    return(
+        <div className="container">
+            <List />
+            <Chat />
+            <Detail />
+        </div>
+
+    )
 }
 
-export default App
+export default App;
