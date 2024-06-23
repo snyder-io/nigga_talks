9c39c459f46453d719a2a8a277b88cb1197a3c6b
Tushar K A <tushar.whitecollar@gmail.com>
2024-06-23 23:17:35 +0530

    NA

--- src/components/list/userInfo/UserInfo.jsx
+++ src/components/list/userInfo/UserInfo.jsx
@@ -3,7 +3,15 @@ import './userInfo.css';
 const UserInfo = () => {
     return (
         <div className="userInfo">
-            UserInfo
+            <div className="user">
+                <img src="./avatar.png" alt="" />
+                <h2>Demo_Name</h2>
+            </div>
+            <div className="icon">
+                <img src="./more.png" alt="" />
+                <img src="./video.png" alt="" />
+                <img src="./edit.png" alt="" />
+            </div>
         </div>
     )
 }
